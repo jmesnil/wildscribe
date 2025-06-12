90ff0deef6c73f49eef7372006dccf1d3b35f869
Jeff Mesnil <jmesnil@redhat.com>
2025-06-12 10:07:43 +0200

    WIP
    Signed-off-by: Jeff Mesnil <jmesnil@redhat.com>

--- wildflydoc/src/main/java/org/wildfly/wildscribe/wildflydoc/SiteGenerator.java
+++ wildflydoc/src/main/java/org/wildfly/wildscribe/wildflydoc/SiteGenerator.java
@@ -9,6 +9,7 @@ import io.quarkus.qute.Template;
 import jakarta.enterprise.context.Dependent;
 import jakarta.inject.Inject;
 import org.jboss.dmr.ModelNode;
+import org.jboss.dmr.Property;
 
 import java.nio.charset.StandardCharsets;
 
@@ -24,11 +25,25 @@ public class SiteGenerator {
         Files.createDirectories(outputDirectory);
 
         try (InputStream in = Files.newInputStream(modelFile)) {
-            ModelNode description = ModelNode.fromJSONStream(in);
-        }
+            ModelNode rootDescription = ModelNode.fromJSONStream(in);
 
-        String content = resource.data("name", "foo").render();
-        Files.writeString(outputDirectory.resolve("foo.html"), content, StandardCharsets.UTF_8);
+            System.out.println(rootDescription.toJSONString(false).substring(0, 500));
+            System.out.println(rootDescription.keys());
+
+            if (rootDescription.has("children", "subsystem", "model-description")) {
+                for (Property prop : rootDescription.get("children", "subsystem", "model-description").asPropertyList()) {
+                    System.out.println("subsystem = " + prop.getName());
+                    String subsystemName = prop.getName();
+                    String description = prop.getValue().get("description").asStringOrNull();
+
+                    String content = resource.data("name", subsystemName,
+                                    "description", description,
+                                    "address", "/subsystem=" + subsystemName)
+                            .render();
+                    Files.writeString(outputDirectory.resolve(subsystemName + ".html"), content, StandardCharsets.UTF_8);
+                }
+            }
+        }
 
         System.out.println("✏️ Site generated at " + outputDirectory);
     }
