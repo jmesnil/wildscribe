247b4b4d2fdba019963281150254718d35fa02b6
Jeff Mesnil <jmesnil@redhat.com>
2025-06-12 08:34:34 +0200

    WIP
    Signed-off-by: Jeff Mesnil <jmesnil@redhat.com>

--- message-dumper/README.md
+++
@@ -1,7 +0,0 @@
-= Message Dumper
-
-```java -jar message-dumper/target/message-dumper.jar /Users/rhusar/STATIC/wf/wildfly-13.0.0.Final/modules/ models/standalone/Wildfly-13.0.0.Final.messages```
-or
-```mvn -pl message-dumper exec:java -Dexec.args="/Users/rhusar/STATIC/wf/wildfly-13.0.0.Final/modules/ models/standalone/Wildfly-13.0.0.Final.messages```
-
-
--- site-generator/src/main/java/org/jboss/wildscribe/site/Main.java
+++ site-generator/src/main/java/org/jboss/wildscribe/site/Main.java
@@ -1,5 +1,6 @@
 package org.jboss.wildscribe.site;
 
+import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class Main {
@@ -10,7 +11,8 @@ public class Main {
                 System.out.println("USAGE: java [-Durl=http://wildscribe.github.io] -jar site-generator.jar model-directory output-directory");
                 System.exit(1);
             }
-            Generator.generate(Paths.get(args[0]), Paths.get(args[1]));
+            Path modelFile = Paths.get(args[0]);
+            Path outputDirectory = Paths.get(args[1]);
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
